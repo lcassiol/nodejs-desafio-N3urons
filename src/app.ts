6ddec4f3ed3c242e78cf86042f6d9cfd0b296521
Cassio C <LccassioL@gmail.com>
2020-08-25 18:22:19 -0300

    Add prettier

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express from 'express';
 
-import cors from "cors";
-import routes from "./routes";
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
