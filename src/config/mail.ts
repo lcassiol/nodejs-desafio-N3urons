b28d084e8e8a3e4f3e4f6075f1cb2ec48a23f149
Cassio C <LccassioL@gmail.com>
2020-08-31 12:00:04 -0300

    Remove unncessary code

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -3,8 +3,8 @@ export default {
   port: 2525,
   secure: false,
   auth: {
-    user: '6ea46cfd73ba2f',
-    pass: 'ebe54b49ba2611',
+    user: '',
+    pass: '',
   },
   default: {
     from: 'Desafio N3urons Agradece <noreply@desafion3urons.com.br>',
